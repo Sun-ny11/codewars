3438cfe1ebb2fe3d218034da6992455f9d13698b
Sun-ny11 <spetrow79@gmail.com>
2023-07-28 11:51:15 +0300

    add basicMathematicalOperations

--- drinkAbout.js
+++ drinkAbout.js
@@ -17,7 +17,7 @@
 function peopleWithAgeDrink(old) {
 
     if(old < 14) {
-        return "drink toddy";
+        retrn "drink toddy";
     }else if(old < 18) {
         return "drink coke";
     }else if(old < 21) { 
