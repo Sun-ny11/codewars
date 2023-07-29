2928482f43007eac4872cc194e1c2ed3b72e1db8
Sun-ny11 <spetrow79@gmail.com>
2023-07-29 09:56:32 +0300

    add willThereBeEnoughSpace

---
+++ willThereBeEnoughSpace.js
@@ -0,0 +1,18 @@
+// Task:
+// Task Overview:
+// You have to write a function that accepts three parameters:
+
+// cap is the amount of people the bus can hold excluding the driver.
+// on is the number of people on the bus excluding the driver.
+// wait is the number of people waiting to get on to the bus excluding the driver.
+// If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
+
+// Solution:
+function enough(cap, on, wait) {
+    if((on + wait) > cap) {
+      return wait - (cap - on)
+    }else {
+      return 0
+    }
+}
