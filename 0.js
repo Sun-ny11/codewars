7fa1cc3660431f67e7404882027436c9c18f4a73
Sun-ny11 <spetrow79@gmail.com>
2023-08-13 11:18:46 +0300

    add 0 nothing

---
+++ 0.js
@@ -0,0 +1,26 @@
+// Task
+// Remove First and Last Character
+
+// It's pretty straightforward.
+// Your goal is to create a function that removes the first and last characters of a string.
+// You're given one parameter, the original string. You don't have to worry with strings with
+// less than two characters.
+
+// Solution:
+
+function removeChar(str){
+    return str.slice(1, -1)
+};
+  
+
+// ------------------------------------------------------------------------------------------
+
+
+// Task
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Solution:
+
+function opposite(number) {
+    return number = -number
+}
