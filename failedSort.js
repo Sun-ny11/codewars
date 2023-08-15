afe1f272b0fa5faeeed1ef832111de48dbf6181f
Sun-ny11 <spetrow79@gmail.com>
2023-08-15 11:31:01 +0300

    add failedSort

---
+++ failedSort.js
@@ -0,0 +1,11 @@
+// Task:
+// Failed Sort - Bug Fixing #4
+// Oh no, Timmy's Sort doesn't seem to be working? 
+// Your task is to fix the sortArray function to sort all numbers in ascending order
+
+
+// Solution:
+
+var sortArray = function(value) {
+    return value.split('').sort((c, p) => c > p ).join('');
+}
